829ff5194dfba857cc0e76740044ea0e54102ba0
mpepe16 <mpepe66@gmail.com>
2022-09-24 15:47:56 +0200

    Add: person ts interface and app.ts

---
+++ app/app.ts
@@ -0,0 +1,13 @@
+let myResult: Result ={
+    playerName: "Maria",
+    score: 5,
+    problemCount: 5,
+    factor: 7
+};
+
+let player : Person = {
+    name: "",
+    formatName: function (): string {
+        throw new Error("Function not implemented.");
+    }
+}
